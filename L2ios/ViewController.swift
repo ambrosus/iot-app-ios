0660a368d2d44b23297158e7e59765d922034daa
Sergei Epatov <sepatov@apptopia.com>
2018-12-09 14:21:27 +0200

    Initial Commit

---
+++ L2ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  L2ios
+//
+//  Created by serj e on 12/9/18.
+//  Copyright © 2018 Ambrosus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ L2iosTests/L2iosTests.swift
@@ -0,0 +1,34 @@
+//
+//  L2iosTests.swift
+//  L2iosTests
+//
+//  Created by serj e on 12/9/18.
+//  Copyright © 2018 Ambrosus. All rights reserved.
+//
+
+import XCTest
+@testable import L2ios
+
+class L2iosTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
