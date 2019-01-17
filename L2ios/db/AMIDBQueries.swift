4c8f1a4b72d34388024a0cb4cbe158b779802de9
Sergei Epatov <sepatov@apptopia.com>
2019-01-18 01:04:10 +0200

    Added: DBQueries (helper class)

--- L2ios/db/AMIDBQueries.swift
+++ L2ios/db/AMIDBQueries.swift
@@ -3,8 +3,31 @@
 //  (c) 2019 Ambrosus. All rights reserved.
 //  
 
-import UIKit
+import Foundation
+import GRDB
 
 class AMIDBQueries: NSObject {
-
+    static var dbQueue: DatabaseQueue? {
+        return AMIDBStarter.sharedInstance.dbQueue
+    }
+    
+    static func fetchDeviceWithUUID(_ uuid:String) -> AMIDeviceRecord? {
+        let result = try! dbQueue?.read{ db in
+            try AMIDeviceRecord.fetchOne(db, "SELECT * FROM device where uuid = ?", arguments:[uuid])
+        }
+        
+        return result
+    }
+    
+    static func saveDeviceRecord(_ record:inout AMIDeviceRecord) {
+        try! dbQueue?.write{ db in
+            if let _ = record.id {
+                try record.update(db)
+            }
+            else {
+                try record.insert(db)
+            }
+        }
+    }
+    
 }
